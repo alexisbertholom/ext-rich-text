24c772a880e8592346715ba4105c8a61e7f6a440
Alexis Bertholom <alexis.bertholom.jd@gmail.com>
2019-03-08 02:02:06 +0900

    feat(build): implement shorthandBuild syntactic sugar function

--- src/build.ts
+++ src/build.ts
@@ -30,3 +30,44 @@ export default function build(...args: ParsedString): string
 {
   return _build(args);
 }
+
+type SHTag = [ string, ...(SHParsedString | string)[] ];
+type SHNode = string | SHTag;
+
+//interface used instance of type to prevent typescript's type aliases circular references
+interface SHParsedString extends Array<SHNode> {};
+
+function isSHTag(node: SHNode): node is SHTag
+{
+  return Array.isArray(node);
+}
+
+function translateSHTag(tag: SHTag): Tag
+{
+  const [ type, ..._args ] = tag;
+  const args = _args.map(translateSHParsedString);
+  return {
+    type,
+    args,
+    node: args.pop() || null,
+  };
+}
+
+function translateSHNode(node: SHNode): Node
+{
+  return isSHTag(node) ? (
+    translateSHTag(node)
+  ) : node;
+}
+
+function translateSHParsedString(args: SHParsedString | string): ParsedString
+{
+  if (typeof(args) === 'string')
+    return [ args ];
+  return args.map(translateSHNode);
+}
+
+export function shorthandBuild(...args: SHParsedString): string
+{
+  return _build(translateSHParsedString(args));
+}
