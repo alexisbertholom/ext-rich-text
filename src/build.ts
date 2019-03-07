831bbfe02b0f3ddcd9699c3a1f4e15fa70bc0b2b
Alexis Bertholom <alexis.bertholom.jd@gmail.com>
2019-03-08 01:40:13 +0900

    feat: implement build(...Node[]) function, which build a rich text string

---
+++ src/build.ts
@@ -0,0 +1,32 @@
+import { Tag, Node, ParsedString, isTag } from './types';
+import escape from './escape';
+
+function formatTag(tag: Tag): string
+{
+  const { type, args, node } = tag;
+  const _args = node ? args.concat([ node ]) : args;
+  const parsedArgs = _args.map(arg => _build(arg));
+  const contents = [ escape(type) ].concat(parsedArgs).join('|');
+  return `[${contents}]`;
+}
+
+function formatNode(node: Node): string
+{
+  return isTag(node) ? (
+    formatTag(node)
+  ) : (
+    escape(node)
+  );
+}
+
+function _build(args: ParsedString)
+{
+  return args.reduce<string>((result: string, node: Node) => (
+    result + formatNode(node)
+  ), '');
+}
+
+export default function build(...args: ParsedString): string
+{
+  return _build(args);
+}
